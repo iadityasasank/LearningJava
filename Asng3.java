c5cb45624d33d1e4fc606853053a88c1fb7fbe0d
iadityasasank <50364371+iadityasasank@users.noreply.github.com>
2019-05-07 16:57:37 +0530

    Itterative 3

---
+++ Asng3.java
@@ -0,0 +1,18 @@
+public class Asng3 {
+
+	public static void main(String args[])
+	{
+		for(int i=4;i<8;i++)
+		{
+			if(i==4||i==7)
+			{
+				System.out.println("$ $ $ $");
+			}
+			else
+			{
+				System.out.println("$     $");
+			}
+			
+		}
+	}
+}

b66933a9612cd8d1b7222f3a73a101b3995eab81
iadityasasank <50364371+iadityasasank@users.noreply.github.com>
2019-05-07 16:55:58 +0530

    Ittirative 1

---
+++ Asng1.java
@@ -0,0 +1,12 @@
+public class Asng2 {
+	public static void main(String args[]) {
+		for(char i =65; i<=68; i++) {
+			for(char j=65; j<=i; j++ )
+			{
+				System.out.print(j);
+			}
+				System.out.println();
+		}
+	}
+
+}

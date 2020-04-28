38c5df33f7e260d0e5619d9f0d772c9775cf501c
iadityasasank <50364371+iadityasasank@users.noreply.github.com>
2020-04-28 20:05:27 +0530

    Add files via upload

--- Car.java
+++ Car.java
@@ -11,7 +11,25 @@ public class Car {
 	
 	String nameOfCar = "Dark Hunter";
 	
+	double maxFuel = 16;
+	double currentFuel = 8;
+	double mpg= 26.4; //Mass per Gallon(Mileage)
 	
+	int Passengers = 1;
+	
+	public Car(int S,double W,boolean C)
+	{
+		minSpeed = S;
+		weight = W;
+		EngineOn = C;
+		
+	}
+	
+	public Car(int S)
+	{
+		minSpeed = S;
+		
+	}
 	
 	
 	public void printValues()
@@ -22,26 +40,59 @@ public class Car {
 		System.out.println("Weight of Car : " + weight);
 		System.out.println("Is Engine On : "+EngineOn);
 		System.out.println("Rating of Car Condition : "+condition);
+		System.out.println("No of Passengers : "+Passengers);
 	}
 	
-	public void wrekCar() {
-		condition ='C';
-	}
+	
 	
 	public void UpgradeMinSpeed() {
 		minSpeed = 10;
 	}
+	
+	public void getIn() {
+		Passengers++;
+	}
+	
+	public void getOut() {
+		Passengers--;
+	}
+	
+	public void FuelLevel()
+	{
+		System.out.println("Maximum Fuel : "+maxFuel);
+		System.out.println("Current Fuel : "+currentFuel);
+	}
+	
+	public double EmptyTankAt(){
+		
+		return mpg*currentFuel;
+	}
+	
+	public double CanTravelMaxAt(){
+		return mpg*maxFuel;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car Mine = new Car();
-		System.out.println("Family Car Details ");
-		Mine.printValues();
-		Car Sandeep = Mine;
-		Mine.wrekCar();
-		Mine.UpgradeMinSpeed();
-		System.out.println("Sandeep's Car Details ");
-		Sandeep.printValues();
+		Car present = new Car(10);
+		Car Serviced = new Car(0,900,false);
+		
+		
+		Serviced.getIn();
+		System.out.println("This is Serviced Car");
+		Serviced.printValues();
+		
+		System.out.println();
+		System.out.println("This is Present Car");
+		present.printValues();
+		
+		System.out.println();
+		System.out.println("Fuel Level in Present Car");
+		present.FuelLevel();
+		
+		
+		
 		
 	}
 
